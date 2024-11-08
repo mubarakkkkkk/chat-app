e0284551007b17e2508a3410d3d21cd3c3d04782
mubarakkkkkk <abiolamubarakolamide05@gmail.com>
2024-11-08 20:38:33 +0100

    switch

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 const App = () => {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-
+   <Router>
+    <div>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-      </Switch>
-    </Router>
-  );
-};
+        <Route path='/'>
 
-export default App;
+        </Route>
+      </Switch>
+    </div>
+   </Router>
+  )
+}
+
+export default App
