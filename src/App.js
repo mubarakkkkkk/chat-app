a1c013bf5df4d73a203568c0ed8fac5e99571268
mubarakkkkkk <abiolamubarakolamide05@gmail.com>
2024-11-09 13:12:12 +0100

    routing sucessful

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom"
+import Home from './Home'
 import CreateAcct from "./Auth/CreateAcct"
 import Forgottenpass from "./Auth/ForgottenPass"
+import Jesica from './Jesica'
 
 const App = () => {
   return (
-   <Router>
-    <div>
-      <Switch>
-        <Route path='/CreateAcct'>
-          <CreateAcct />
-        </Route>
-      </Switch>
-      <Switch>
-        <Route path='/Forgottenpass'>
-          <Forgottenpass />
-        </Route>
-      </Switch>
-    </div>
-   </Router>
+    <Router>
+      <div>
+        {/* <nav>
+          <Link to="/">App</Link> | <Link to="/createAcct">Create Account</Link> |{" "}
+          <Link to="/forgottenpass">Forgot Password</Link> |{" "}
+          <Link to="/jesica">Jesica</Link>
+        </nav> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createAcct" element={<CreateAcct />} />
+          <Route path="/forgottenpass" element={<Forgottenpass />} />
+          <Route path="/jesica" element={<Jesica />} />
+          {/* Optional 404 Not Found route */}
+          <Route path="*" element={<div>404 - Page Not Found</div>} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
