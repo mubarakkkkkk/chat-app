67b44afdd5467564e757eaf828fde8d93140d024
mubarakkkkkk <abiolamubarakolamide05@gmail.com>
2024-11-09 18:32:38 +0100

    logo process

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom"
 import Home from './Home'
-import CreateAcct from "./Auth/CreateAcct"
+import SignUp from "./Auth/SignUp"
 import Forgottenpass from "./Auth/ForgottenPass"
-import Jesica from './Jesica'
+import Login from './Auth/Login'
 
 const App = () => {
   return (
     <Router>
       <div>
-        {/* <nav>
-          <Link to="/">App</Link> | <Link to="/createAcct">Create Account</Link> |{" "}
-          <Link to="/forgottenpass">Forgot Password</Link> |{" "}
-          <Link to="/jesica">Jesica</Link>
-        </nav> */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/createAcct" element={<CreateAcct />} />
-          <Route path="/forgottenpass" element={<Forgottenpass />} />
-          <Route path="/jesica" element={<Jesica />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgottenpassword" element={<Forgottenpass />} />
+          <Route path="/login" element={<Login />} />
           {/* Optional 404 Not Found route */}
           <Route path="*" element={<div>404 - Page Not Found</div>} />
         </Routes>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+
 const Home = () => {
   return (
     <div>
       <h1>
-        this is the test 
+       ConvoHub
       </h1>
+      <img src="/public/convohub-logo-v4.svg" alt="App logo" style={{ width: '200px', height: 'auto' }} />
     </div>
   )
 }
