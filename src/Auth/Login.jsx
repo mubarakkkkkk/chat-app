aa9e3e1eb9b5cc24e384053ce89c5cd0c88c87b1
mubarakkkkkk <abiolamubarakolamide05@gmail.com>
2024-11-15 20:32:12 +0100

    forgotten password

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
+import '../index.css'
 
 const Login = () => {
+
   return (
     <div>
       <div className='loginContainer'>
-        <h1>
+        <h1 className='welcomeTxt'>
             Welcome Back
         </h1>
         <div className='googleAndApple'>
-            <button>
+            <button className='continueWithGoogle'>
               Conntinue With Google
             </button>
-            <button>
+            <button className='continueWithApple'>
               Conntinue With Apple
             </button>
         </div>
         <div className='auth'>
-            <input type="text" placeholder='Enter Email or Username' />
-            <input type="password" placeholder='Enter Password' />
-            <button>Login</button>
-            <a href="/forgottenPass">Forgot password ?</a>
+            <input type="text" placeholder='Enter Email or Username' className='userId'/>
+            <input type="password" placeholder='Enter Password' className='password'/>
+            <button className='LoginBtn'>Login</button>
+            <a href="/forgottenPass" className='forgottenPasswordLink'>Forget password ?</a>
         </div>
       </div>
     </div>
